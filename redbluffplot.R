2b5d8c79341cfab2e47b0838a9b6c0ca09a6f200
Hartman <Rosemary.Hartman@water.ca.gov>
2019-09-10 15:51:45 -0700

    new layout and fixing redbluff graphs

--- redbluffplot.R
+++ redbluffplot.R
@@ -70,7 +70,7 @@ theme_iep <- function(){
           panel.grid.minor.y = element_blank(),
           panel.grid.major.y = element_blank(),  
           #plot.margin = unit(c(0.1, 0.3, 0.1, 0.9), units = , "cm"), #top, right, bottom, left
-          plot.margin = unit(c(0.25, 0.4, 0.1, 0.4), units = , "cm"), #adjusted the "top" and "right" values so nothing is cut off
+          #plot.margin = unit(c(0.25, 0.4, 0.1, 0.4), units = , "cm"), #adjusted the "top" and "right" values so nothing is cut off
           plot.title = element_text(size = 20, vjust = 1, hjust = 0.5),
           legend.text = element_text(size = 9, face = "plain"),
           legend.title=element_text(size=10))
@@ -78,5 +78,8 @@ theme_iep <- function(){
 
 #We just wanted winter run for the winter report
 Red = ggplot(filter(meanwinter, runname == "Winter.Chinook.Passage.Estimate"), aes(x = Year2, y = meandaily))
-Red + geom_bar(stat = "identity") + ylab("Estimated dialy passage") + xlab("Year (December-January)") +
-  theme_iep()
+redtest = Red + geom_bar(stat = "identity") + ylab("Estimated dialy passage") + xlab("Year (December-January)") +
+  coord_cartesian(xlim = c(1966, 2018), ylim = c(0, 4300))+
+  theme_iep() + annotate("text", label = ("Data were not collected until 2004"), x = 1980, y = 100, size = 2.5)
+
+ggsave(redtest, file="redtest.png", dpi=300, units="cm", width=9.3, height=6.8)
