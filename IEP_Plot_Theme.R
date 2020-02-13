c488227411dd22e50f3856a016af4fd292534135
mountaindboz <David.Bosworth@water.ca.gov>
2020-02-13 11:13:19 -0800

    Removed extra margin space between axis and tick labels for y-axis

--- IEP_Plot_Theme.R
+++ IEP_Plot_Theme.R
@@ -12,16 +12,11 @@ theme_iep <- function() {
   theme(
     # Define text size of tick labels
     axis.text = element_text(size = 9),
-    # Define text size and face of axes labels; modify margins of y-axis label
-    axis.title.x = element_text(
+    # Define text size and face of axes labels
+    axis.title = element_text(
       size = 10, 
       face = "plain"
     ),
-    axis.title.y = element_text(
-      size = 10,
-      face = "plain",
-      margin = margin(0, 10, 0, 0) #top, right, bottom, left
-    ), 
     # Remove panel grid lines
     panel.grid = element_blank(),
     # Adjust the margin dimensions so nothing is cut off
