0291204d2e40ad86d0df516a5072293ffc4e4987
mountaindboz <David.Bosworth@water.ca.gov>
2020-02-13 09:59:37 -0800

    Cleaned up code for theme_iep function

--- IEP_Plot_Theme.R
+++ IEP_Plot_Theme.R
@@ -1,21 +1,43 @@
+# Seasonal Monitoring Report
+# Purpose: A group of common functions to apply consistency across plots
+# Author: Dave Bosworth
 
-## Create custom plot formatting function
-theme_iep <- function(){
-  theme_bw()+
-    theme(
-      axis.text.x = element_text(size = 9),
-      axis.text.y = element_text(size = 9),
-      axis.title.x = element_text(size = 10, face = "plain"),
-      axis.title.y = element_text(size = 10, face = "plain",
-                                  margin=margin(t = 0, r = 10, b = 0, l = 0)), 
-      panel.grid.major.x = element_blank(),
-      panel.grid.minor.x = element_blank(),
-      panel.grid.minor.y = element_blank(),
-      panel.grid.major.y = element_blank(),  
-      #plot.margin = unit(c(0.1, 0.3, 0.1, 0.9), units = , "cm"), #top, right, bottom, left
-      plot.margin = unit(c(0.25, 0.6, 0.1, 0.4), units = "cm"), #adjusted the "top" and "right" values so nothing is cut off
-      plot.title = element_text(size = 20, vjust = 1, hjust = 0.5),
-      legend.text = element_text(size = 9, face = "plain"),
-      legend.title=element_text(size=10)
-    )
+
+# Custom plot formatting function
+theme_iep <- function() {
+  theme_bw() +
+  theme(
+    # Define text size of tick labels
+    axis.text = element_text(size = 9),
+    # Define text size and face of axes labels; modify margins of y-axis label
+    axis.title.x = element_text(
+      size = 10, 
+      face = "plain"
+    ),
+    axis.title.y = element_text(
+      size = 10,
+      face = "plain",
+      margin = margin(0, 10, 0, 0) #top, right, bottom, left
+    ), 
+    # Remove panel grid lines
+    panel.grid = element_blank(),
+    # Adjust the margin dimensions so nothing is cut off
+    plot.margin = unit(
+      c(0.25, 0.6, 0.1, 0.4), #top, right, bottom, left
+      units = "cm"
+    ),
+    # Define text size and justification of plot title
+    plot.title = element_text(
+      size = 20, 
+      vjust = 1, 
+      hjust = 0.5
+    ),
+    # Define text size and face of legend item labels
+    legend.text = element_text(
+      size = 9, 
+      face = "plain"
+    ),
+    # Define text size of legend title
+    legend.title=element_text(size = 10)
+  )
 }
