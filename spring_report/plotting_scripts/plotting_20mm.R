a22e38a769aba1770375e608564217dba5a13e2b
lmitchell4 <lmitchell4@gmail.com>
2020-02-18 21:53:49 -0800

    Regenerate figures using new smonitr functions

--- spring_report/plotting_scripts/plotting_20mm.R
+++ spring_report/plotting_scripts/plotting_20mm.R
@@ -1,33 +1,38 @@
 
 library(ggplot2)
 
+report_year <- 2018
+
 projectRoot <- "."
 reportRoot <- file.path(projectRoot,"spring_report")
 dataRoot <- file.path(projectRoot,"data")
 thisDataRoot <- file.path(dataRoot,"20mm")
 figRoot <- file.path(reportRoot,"figures")
 
-source(file.path(projectRoot, "IEP_Plot_Theme.R"))
+source(file.path(projectRoot,"smonitr","R","plot_tools.R"))
 
 ##########################################################################
 ## Read in index data:
 
-dsmIndexDf <- read.csv(file.path(thisDataRoot,"20mm_DSM_index.csv"),
-											 stringsAsFactors=FALSE)
-lfsIndexDf <- read.csv(file.path(thisDataRoot,"20mm_LFS_index.csv"), 
-											 stringsAsFactors=FALSE)
+dsmIndexDf_raw <- read.csv(file.path(thisDataRoot,"20mm_DSM_index.csv"),
+                           stringsAsFactors=FALSE)
+lfsIndexDf_raw <- read.csv(file.path(thisDataRoot,"20mm_LFS_index.csv"), 
+                           stringsAsFactors=FALSE)
 
+## Truncate the data according to the specified report year:
+dsmIndexDf <- subset(dsmIndexDf_raw, Year <= report_year)
+lfsIndexDf <- subset(lfsIndexDf_raw, Year <= report_year)
 
 ##########################################################################
 ## Delta Smelt
 
 dsm_fig <- ggplot(dsmIndexDf, aes(x=Year, y=Index))+
   geom_bar(stat="identity") +
-  theme_iep() +
+  theme_smr() + 
   theme(legend.position="none") + 
   scale_y_continuous(expression(paste("Index"))) + 
-  geom_hline(yintercept=mean(dsmIndexDf$Index, na.rm=TRUE), col="red", 
-             linetype="dashed", size=0.9)
+  lt_avg_line(lt_avg=mean(dsmIndexDf$Index, na.rm=TRUE)) + 
+  std_x_axis_all_years(rpt_yr=report_year, start_yr=min(dsmIndexDf$Year))
 
 ggsave(dsm_fig, file=file.path(figRoot,"20mm_DSM.png"), dpi=300, units="cm", 
 			 width=9.3, height=6.8)
@@ -38,11 +43,11 @@ ggsave(dsm_fig, file=file.path(figRoot,"20mm_DSM.png"), dpi=300, units="cm",
 
 lfs_fig <- ggplot(lfsIndexDf, aes(x=Year, y=Index))+
   geom_bar(stat="identity") +
-  theme_iep() +
+  theme_smr() + 
   theme(legend.position="none") + 
   scale_y_continuous(expression(paste("Index"))) + 
-  geom_hline(yintercept=mean(lfsIndexDf$Index, na.rm=TRUE), col="red", 
-             linetype="dashed", size=0.9)
+  lt_avg_line(lt_avg=mean(dsmIndexDf$Index, na.rm=TRUE)) + 
+  std_x_axis_all_years(rpt_yr=report_year, start_yr=min(lfsIndexDf$Year))
 
 ggsave(lfs_fig, file=file.path(figRoot,"20mm_LFS.png"), dpi=300, units="cm", 
 			 width=9.3, height=6.8)
