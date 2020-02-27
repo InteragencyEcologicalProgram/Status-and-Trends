8d9e9f9adf15ce977ef8464a75dadc088b9b3047
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2020-02-27 12:33:12 -0800

    specified a more specific path for saving plot so now it gets saved in the 'summer_report' subfolder

--- summer_report/SeasonalMonitoringReport_Summer_AquaticVegetation.R
+++ summer_report/SeasonalMonitoringReport_Summer_AquaticVegetation.R
@@ -1,92 +1,80 @@
 #Seasonal Monitoring Report
 #Summer 2018
-#Invasive aquatic vegetation: North + Central Delta only
-#Coveraged based on hyperspectral imagery
+
+#Purpose: Create plot of invasive aquatic vegetation coverage through time
+#Data represent North + Central Delta only
+#Coverage estimates based on hyperspectral imagery
+#Data for submerged and floating aquatic vegetation types
 #Data acquired from Shruti Khanna (CDFW)
+
+#Author: Nick Rasmussen 
 
 #required packages
 library(tidyverse) #plotting data; converting between long and wide data frames
-library(colorspace) #plot color palettes
 library(scales) #modifying y axis to include '%'
 library(devtools) #needed for installing 'smonitr' from GitHub
 
-#install most up to date version of 'smonitr' from GitHub
-devtools::install_github("InteragencyEcologicalProgram/Status-and-Trends/smonitr")
-library(smonitr) 
+#install and load most up to date version of 'smonitr' from GitHub
+#devtools::install_github("InteragencyEcologicalProgram/Status-and-Trends/smonitr")
+library(smonitr) #standardizes formatting of plots
 
 
-#import data
-veg<-read.csv("veg.csv")
-#sharepoint location
-#https://cawater.sharepoint.com/:x:/r/sites/dwr-str/Shared%20Documents/Data/veg.csv?d=wdb4811ae33cf4e5ca21ae1fd36ebf5cd&csf=1&e=Ee6pdO
-str(veg)
-
-#working directory
-setwd("C:/Users/nrasmuss/OneDrive - California Department of Water Resources/IEP/IEP_EstuaryIndicesProject/Report_Summer")
-#setwd("C:/Repositories/Status-and-Trends/summer_report")
+# 1. Import Data ----------------------------------------------------------
 
 
+# Dataset is on SharePoint site for the Seasonal Monitoring Report
 
-#create subset with acres 
-#alist<-c("year","sav_acres", "wp_acres",  "wh_acres",  "fav_acres")
-#acr<-veg[,alist]
+# Define path on SharePoint site for data
+sharepoint_path <- normalizePath(
+  file.path(
+    Sys.getenv("USERPROFILE"),
+    "California Department of Water Resources/IEP Synthesis Projects - Data"
+  )
+)  
 
-#convert from wide to long
-#acrl<-gather(acr,type,acres,sav_acres:fav_acres,factor_key=T)
+# Import vegetation data
+veg<-read.csv(file = paste0(sharepoint_path, "/AquaticVegetation_North+CentralDelta.csv"))
 
-#create subset that is just total SAV & FAV
-#excludes WH and WP data
-#sacrl<-subset(acrl,type=="sav_acres" | type=="fav_acres")
+#examine data frame
+str(veg) #looks fine
 
-#create subset that is just total SAV, WH, and WP
-#excludes summarized FAV column
-#spp<-subset(acrl,type!="fav_acres")
+
+#2. Data frame manipulations in preparation for plotting-------------------
+
 
 #create subset with percent coverage of waterways
 plist<-c("year","sav_perc","wp_perc",   "wh_perc",   "fav_perc" )
 perc<-veg[,plist]
 
-#convert from wide to long
+#convert data frame from wide to long
 percl<-gather(perc,type,perc,sav_perc:fav_perc,factor_key=T)
 
 #create subset that is just total SAV & FAV
 #excludes WH and WP data
 ppercl<-subset(percl,type=="sav_perc" | type=="fav_perc")
 
-#stacked bar plot of acres: FAV and SAV
+#create subset with acres 
+#alist<-c("year","sav_acres", "wp_acres",  "wh_acres",  "fav_acres")
+#acr<-veg[,alist]
 
-#(pacr<-ggplot(sacrl,aes(x=year, y=acres))+
-#  geom_col(aes(fill=type))+theme_iep()
-#  )
-#ggsave("veg_acres.png",scale=0.7, height=8,width=11, units="in",dpi=300)
+#convert data frame from wide to long
+#acrl<-gather(acr,type,acres,sav_acres:fav_acres,factor_key=T)
+
+#create subset that is just total SAV & FAV
+#excludes WH and WP data
+#sacrl<-subset(acrl,type=="sav_acres" | type=="fav_acres")
+
+#create subset that is just SAV, WH, and WP
+#excludes summarized FAV column
+#spp<-subset(acrl,type!="fav_acres")
 
 
-#stacked bar plot of percent coverage
-
-#create df that will indicate where the missing data point labels will be positioned on the graph
-
-#first create df with all possible years and veg type levels
-#yrs<-seq(2004,2018,1)
-#vt<-c("sav_perc","fav_perc")
-#aveg<-data.frame(expand.grid(yrs,vt))
-#names(aveg)<-c("year","type")
-#str(aveg)
-
-#use anti-join to combine the complete 'aveg' df with 'ppercl' df which excludes missing data
-#this will create a new df with just the combinations with missing data
-#md<-anti_join(aveg,ppercl) #throws a warning but df looks fine
-#str(md)
-
-#add a new column to 'md' df that will provides the number representing the position of the '*' on the y axis
-#should be low (e.g., 0, 1, or 2)
-#md$y<-"0"
-#md$y<-as.numeric(md$y)
-#str(md)
+#3. Set up options for plots------------------------------
 
 #calcuate mean for total veg % coverage (SAV + FAV) across all years
 #then plot this as a horizontal red dashed line
 
-#first add FAV and SAV within each year
+#first sum FAV and SAV within each year
 vtot<-setNames(aggregate(x=ppercl$perc,by=list(ppercl$year),FUN=sum), c("year","perc"))
 str(vtot)
 mean(vtot$perc) #29.6
@@ -139,21 +127,54 @@ missing_data_symb <- function(df, yr_var, rpt_yr) {
 }
 
 
-(pperc<-ggplot(ppercl,aes(x=year, y=perc,fill=type))+
+#4. Create plots---------------------------------------------
+
+
+#stacked bar plot of percent coverage
+pperc<-ggplot(ppercl,aes(x=year, y=perc,fill=type))+
+   #specifies the dependent and independent variables 
     geom_bar(position = "stack", stat = "identity",colour="grey25")+
+   #specifies that this is a stacked bar plot
     ylab("Water area occupied") + xlab("Year")+
+   #axis labels
     scale_y_continuous(labels= function(x) paste0(x, "%"))+
-    scale_fill_manual(name = "",labels=c("Floating","Submerged")
-                      ,values=repcols)+
+   #adds a percent sign after each y axis tick label numbers
+    scale_fill_manual(name = "",labels=c("Floating","Submerged"),values=repcols)+
+   #customizes names in legend key and specifies the custom color palette
+  theme_smr()+
+  #implements standardized plot formatting
     theme(legend.position=c(0.38,0.85),legend.direction="horizontal")+
+   #specifies location of legend and that elements of legend should be arranged horizontally
     missing_data_symb(ppercl,year,2018)+
-    theme_smr()+
+   #adds symbols anywhere there is missing data
     lt_avg_line(mean(vtot$perc))+
+   #adds horizontal line to plot to indicate long term average for data
     std_x_axis_rec_years(2018)
-)
-#ggsave("veg_perc.png", height=6.8,width=9.3, units="cm",dpi=300)
+   #standardizes the x axis range
 
 
+#print plot
+
+#path to location to put plot
+plot_path<-"C:/Repositories/Status-and-Trends/summer_report"
+
+ggsave(
+  plot=pperc,
+  filename = "veg_perc.png", 
+  dpi=300,
+  units="cm",
+  width=9.3,
+  height=6.8,
+  path = plot_path 
+  )
+
+
+#stacked bar plot of acres: FAV and SAV
+
+#(pacr<-ggplot(sacrl,aes(x=year, y=acres))+
+#  geom_col(aes(fill=type))+theme_iep()
+#  )
+#ggsave("veg_acres.png",scale=0.7, height=8,width=11, units="in",dpi=300)
 
 
 
