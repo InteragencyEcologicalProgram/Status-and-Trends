5a1847aa1dbaf9fa65eaf2de5ccfae9676693750
mountaindboz <David.Bosworth@water.ca.gov>
2020-03-02 15:41:01 -0800

    Adding a function to standardize x-axis labels.

--- smonitr/R/plot_tools.R
+++ smonitr/R/plot_tools.R
@@ -3,6 +3,7 @@
 #'     ggplot theme and removes plot gridlines and modifies the text size of
 #'     various ggplot elements.
 #'
+#' @return A ggplot layer that applies a custom theme as described above.
 #' @import ggplot2
 #' @export
 theme_smr <- function() {
@@ -47,6 +48,7 @@ theme_smr <- function() {
 #'     user is required to input the long-term average value into the function.
 #'
 #' @param lt_avg The long-term average of the data
+#'
 #' @return A horizontal dashed red line to a ggplot
 #' @import ggplot2
 #' @export
@@ -67,6 +69,7 @@ lt_avg_line <- function(lt_avg) {
 #' @param num Integer to round
 #' @param mult Mulitple value to round to
 #' @keywords internal
+#'
 #' @return A value rounded to the nearest multiple of the \code{mult} parameter
 round_to_mult <- function(num, mult) {
   num_round <- round(num/mult) * mult
@@ -99,7 +102,7 @@ std_x_axis_all_years <- function(rpt_yr,
                                  x_scale_type = c("discrete", "cont"),
                                  start_yr = 1966,
                                  break_int = 10) {
-  # evaluate choices for scale_type
+  # Evaluate choices for scale_type
   x_scale_type <- match.arg(x_scale_type, c("discrete", "cont"))
 
   # Define year breaks
@@ -137,6 +140,7 @@ std_x_axis_all_years <- function(rpt_yr,
 #'     with \code{geom_col} geoms are "discrete" scale type, and \code{geom_line}
 #'     \code{geom_area} geoms are "cont" or continuous scale type. Default is
 #'     \code{x_scale_type = "discrete"}.
+#'
 #' @return A ggplot layer that defines the x-axis limits and breaks for plots of
 #'     recent data. The default limits are \code{rpt_yr - 14} to \code{rpt_yr},
 #'     and the default break interval is 5 years.
@@ -146,6 +150,36 @@ std_x_axis_rec_years <- function(rpt_yr, x_scale_type = c("discrete", "cont")) {
   std_x_axis_all_years(rpt_yr, x_scale_type, start_yr = rpt_yr - 14, break_int = 5)
 }
 
+
+#' @title Add a standardized x-axis label to a ggplot
+#' @description Adds a standardized x-axis label to a ggplot based on the season
+#'     of the report.
+#'
+#' @param season The user defined season for the Seasonal Monitoring Report. Must be
+#'     one of the following: \code{"winter", "spring", "summer", "fall", "annual"}.
+#'
+#' @return A standardized x-axis label to a ggplot object. Each season option
+#'     provides a label in the format "Year (Month - Month)". The argument
+#'     \code{season = "annual"} provides the label "Year" without specified months.
+#' @importFrom dplyr case_when
+#' @import ggplot2
+#' @export
+std_x_axis_label <- function(season = c("winter", "spring", "summer", "fall", "annual")) {
+  # Evaluate choices for season arg
+  season <- match.arg(season, c("winter", "spring", "summer", "fall", "annual"))
+
+  # Define x-axis label depending on the season arg
+  x_axis_label <- case_when(
+    season == "winter" ~ "Year (December - February)",
+    season == "spring" ~ "Year (March - May)",
+    season == "summer" ~ "Year (June - August)",
+    season == "fall" ~ "Year (September - November)",
+    TRUE ~ "Year"
+  )
+
+  # Add layer to ggplot object
+  xlab(x_axis_label)
+}
 
 #' @title Add symbols to a ggplot representing missing data
 #' @description Adds tan triangle symbols to a ggplot to represent years with
