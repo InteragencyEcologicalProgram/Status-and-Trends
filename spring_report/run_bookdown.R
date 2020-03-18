0e67f653edc00f73fceb5d349a8a6332eda0433a
rosehartman <rosehartman@gmail.com>
2020-03-18 14:34:01 -0700

    im so confused

--- spring_report/run_bookdown.R
+++ spring_report/run_bookdown.R
@@ -8,6 +8,6 @@ reportRoot <- file.path(projectRoot,"spring_report")
 
 ## Seems like bookdown works better if you change the working directory:
 setwd(file.path(reportRoot,"report_bookdown"))
-bookdown::render_book(input="index.Rmd")
+bookdown::render_book(input="index.Rmd", output_dir = "C:/Users/rhartman/OneDrive - California Department of Water Resources/status and trends/Status-and-Trends/docs")
 bookdown::publish_book(account = "Rosemary_Hartman", render = 'local')
 
