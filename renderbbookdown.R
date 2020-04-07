9a878a74c16ed358bd2c849e279cefd5dbd28a0d
rosehartman <rosehartman@gmail.com>
2020-04-07 12:57:31 -0700

    updating book

---
+++ renderbbookdown.R
@@ -0,0 +1,12 @@
+
+library(bookdown)
+library(png)
+library(grid)
+
+projectRoot <- "."
+
+## Seems like bookdown works better if you change the working directory:
+setwd(file.path(projectRoot,"report_bookdown"))
+bookdown::render_book(input="index.Rmd", 
+                      output_dir =file.path("..","docs"))
+bookdown::publish_book(account = "Rosemary_Hartman", render = 'local')
