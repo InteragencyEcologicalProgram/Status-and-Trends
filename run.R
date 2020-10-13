247003f96514ecaa32f32e8d3514ad30293d6a1b
rosehartman <rosehartman@gmail.com>
2020-10-13 09:06:28 -0700

    get rid of report folder because I renamed it docs

--- run.R
+++ run.R
@@ -70,7 +70,7 @@ fig_root_winter <- file.path(fig_root,"winter")
 
 ## Seems like bookdown works better if you change the working directory:
 setwd(bookdown_root)
-render_book(input="index.Rmd",  output_dir=file.path(root,"report"))
+render_book(input="index.Rmd",  output_dir=file.path(root,"docs"))
 #publish_book(account="Rosemary_Hartman", render="local")
 #render_book(input="index.Rmd", output_format="all", output_dir=file.path(root,"report"))
 setwd(root)
